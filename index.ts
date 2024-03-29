46fbb6c1a1220e08490b12447c42704c74bc2a45
Duc Dang <duc82>
2024-03-29 13:49:16 +0700

    version 1.0.0

--- index.ts
+++
@@ -1,3 +0,0 @@
-import InfiniteScroll from "./src/components/InfiniteScroll";
-
-export default InfiniteScroll;
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
-import { defineConfig } from "vite";
+import { defineConfig, splitVendorChunkPlugin } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import dts from "vite-plugin-dts";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   build: {
+    sourcemap: true,
+    emptyOutDir: true,
     lib: {
-      entry: path.resolve(__dirname, "./index.ts"),
+      entry: path.resolve(__dirname, "./src/lib/index.ts"),
       name: "react-infinite-scroll",
       fileName: (format) => `index.${format}.js`,
     },
@@ -20,9 +23,12 @@ export default defineConfig({
         },
       },
     },
-    sourcemap: true,
-    emptyOutDir: true,
   },
 
-  plugins: [react(), dts({ rollupTypes: true })],
+  plugins: [
+    react(),
+    libInjectCss(),
+    dts({ insertTypesEntry: true, include: ["./src/lib"] }),
+    splitVendorChunkPlugin(),
+  ],
 });
